ef815ae0ed5dbf018998dd937ad18105005fc1a6
giuseppecastaldo <57662742+giuseppecastaldo@users.noreply.github.com>
2020-05-19 15:52:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,10 +28,8 @@ def fb_receive_message():
     return "Hi"
 
 def send_message(recipient_id, message_text):
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
     params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+        "access_token": os.environ[FB_TOKEN]
     }
     headers = {
         "Content-Type": "application/json"
@@ -45,21 +43,6 @@ def send_message(recipient_id, message_text):
         }
     })
     r = requests.post("https://graph.facebook.com/v2.9/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
-
-def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
-    try:
-        if type(msg) is dict:
-            msg = json.dumps(msg)
-        else:
-            msg = unicode(msg).format(*args, **kwargs)
-        print("{}: {}".format(datetime.now(), msg))
-    except UnicodeEncodeError:
-        pass  # squash logging errors in case of non-ascii text
-    sys.stdout.flush()
 
 if __name__ == '__main__':
     app.run()
