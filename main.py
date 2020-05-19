3b0fcc08ba773eaf01452776c76f5400fa4de3d5
giuseppecastaldo <57662742+giuseppecastaldo@users.noreply.github.com>
2020-05-20 00:03:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 from flask import Flask, request
 
 app = Flask(__name__)
-FB_TOKEN = 'EAAD4hzOfxVcBAIdn3KZCjGA4olFf2Ytw67QZA0FeCtj3javQrn8UGwUDO7M6xZAx3ZBy20vk4wujhH7y7uPHQgKXCMSsu5q1pvj01WodzqWgXgAdjLxhITlCyc03G8gYauZCZCkZBIU36BkptRHi9L9ApHgKK3kdp3OAUGtSpZCGtgZDZD'
+PAT = 'EAAD4hzOfxVcBAIdn3KZCjGA4olFf2Ytw67QZA0FeCtj3javQrn8UGwUDO7M6xZAx3ZBy20vk4wujhH7y7uPHQgKXCMSsu5q1pvj01WodzqWgXgAdjLxhITlCyc03G8gYauZCZCkZBIU36BkptRHi9L9ApHgKK3kdp3OAUGtSpZCGtgZDZD'
 
 @app.route('/', methods=["GET"])
 def fb_webhook():
@@ -15,35 +15,41 @@ def fb_webhook():
     if verification_code == verify_token:
         return request.args.get('hub.challenge')
 
-@app.route('/', methods=["POST"])
-def fb_receive_message():
-    for entry in data["entry"]:
-        for messaging_event in entry["messaging"]:
-            if messaging_event.get("message"):  # someone sent us a message
-                sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                message_text = messaging_event["message"]["text"]  # the message's text
+@app.route('/', methods=['POST'])
+def webhook():
+    req = request.get_json()
+    log(req) 
+    if req['object'] == ('page'):
+        for entry in req['entry']:
+            for messaging_event in entry['messaging']:
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id'] 
+ 
+                if messaging_event.get('message'):
+                    if ('text') in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text'] 
+                        sendMessage(PAT,sender_id,messaging_text)
+                    else:
+                        messaging_text = ('no text')
+    return ("ok", 200)
 
-                send_message(sender_id, "roger that!")
-                    
-    return "Hi"
-
-def send_message(recipient_id, message_text):
+def sendMessage(token, sender_id, text):
+    json_data = {
+        "recipient": {"id": sender_id},
+        "message": {"text": text }
+    }
     params = {
-        "access_token": os.environ[FB_TOKEN]
+        "access_token": token
     }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v7.0/me/messages", params=params, headers=headers, data=data)
-
+ 
+    r = requests.post('https://graph.facebook.com/v7.0/me/messages', json=json_data, params=params)
+    
+    if r.status_code != requests.codes.ok:
+       print (r.text)
+    
+def log(message):
+    print(message)
+    sys.stdout.flush()
+    
 if __name__ == '__main__':
     app.run()
