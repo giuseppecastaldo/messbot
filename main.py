817bb32d01c8ed24eb8c3d79a8cb70f74628c93c
giuseppecastaldo <57662742+giuseppecastaldo@users.noreply.github.com>
2020-05-19 15:49:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import os
 import sys
 import json
 from datetime import datetime
-
 import requests
 from flask import Flask, request
-from pymessager.message import Messager
 
 app = Flask(__name__)
-client = Messager('EAAD4hzOfxVcBAIdn3KZCjGA4olFf2Ytw67QZA0FeCtj3javQrn8UGwUDO7M6xZAx3ZBy20vk4wujhH7y7uPHQgKXCMSsu5q1pvj01WodzqWgXgAdjLxhITlCyc03G8gYauZCZCkZBIU36BkptRHi9L9ApHgKK3kdp3OAUGtSpZCGtgZDZD')
+FB_TOKEN = 'EAAD4hzOfxVcBAIdn3KZCjGA4olFf2Ytw67QZA0FeCtj3javQrn8UGwUDO7M6xZAx3ZBy20vk4wujhH7y7uPHQgKXCMSsu5q1pvj01WodzqWgXgAdjLxhITlCyc03G8gYauZCZCkZBIU36BkptRHi9L9ApHgKK3kdp3OAUGtSpZCGtgZDZD')
 
 @app.route('/', methods=["GET"])
 def fb_webhook():
@@ -25,9 +23,43 @@ def fb_receive_message():
             sender_id = message['sender']['id']
             recipient_id = message['recipient']['id']
            
-            client.send_text(recipient_id, "Hello, I'm bot.")
+            send_message(recipient_id, "Hello, I'm bot.")
                 
     return "Hi"
+
+def send_message(recipient_id, message_text):
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.9/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
+    try:
+        if type(msg) is dict:
+            msg = json.dumps(msg)
+        else:
+            msg = unicode(msg).format(*args, **kwargs)
+        print("{}: {}".format(datetime.now(), msg))
+    except UnicodeEncodeError:
+        pass  # squash logging errors in case of non-ascii text
+    sys.stdout.flush()
 
 if __name__ == '__main__':
     app.run()
