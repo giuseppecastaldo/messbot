1bffd05b6d4a300a4979539385ddc7d0d8714417
Giuseppe Castaldo <peppopiano@gmail.com>
2020-05-20 18:02:26 +0200

    test buttons list

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ def webhook():
                 if messaging_event.get('message'):
                     if ('text') in messaging_event['message']:
                         messaging_text = messaging_event['message']['text']
-                        bot.set_get_started_button()
-                        bot.set_persistent_menu()
+                        bot.send_message(sender_id, sender_id)
                 #if messaging_event.get('postback'):
                     #if messaging_event['postback']['payload'] == 'ciao':
                         #sendMessage(PAT,sender_id,'Hai premuto il pulsante 1')
