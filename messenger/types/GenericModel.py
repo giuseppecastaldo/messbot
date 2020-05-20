7cb397ea2749c00ed07d957414bb34cd527e7476
Giuseppe Castaldo <peppopiano@gmail.com>
2020-05-20 15:04:11 +0200

    test buttons list

--- messenger/types/GenericModel.py
+++ messenger/types/GenericModel.py
@@ -1,7 +1,8 @@
+import json
 class GenericModel:
 
     def __init__(self, title, subtitle, image_url, buttons):
         self.title = title
         self.subtitle = subtitle
         self.image_url = image_url
-        self.buttons = buttons
+        self.buttons = json.dumps([button.__dict__ for button in buttons])
